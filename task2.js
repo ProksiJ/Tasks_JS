17e3ff77904e9f9ced17a83407a0c2a7ab63bcba
Tomasheva Ksenia <82093959+ProksiJ@users.noreply.github.com>
2021-07-08 22:43:55 +0300

    use filter

--- task2.js
+++ task2.js
@@ -1,13 +1,3 @@
 const words = ['some', 'thing', 'is', 'here', 'in', 'array']
-const allowedWord = 'herethingarray'
-let newWords = []
-
-for(let i = 0; i<words.length; i++){
-    if(!allowedWord.includes(words[i])){
-        newWords.push(words[i])
-    }
-}
-
-newWords.forEach(element => {
-    console.log(element)
-});
+const allowedWords = ['some', 'is']
+console.log(words.filter(word => allowedWords.includes(word)))
