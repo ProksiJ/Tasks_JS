4d46c054fb9f419ffbf5de6466e565b1a04cb167
Tomasheva Ksenia <82093959+ProksiJ@users.noreply.github.com>
2021-07-08 23:04:43 +0300

    updated

--- task4.js
+++ task4.js
@@ -1,8 +1,2 @@
 const string = 'a,b,c,d,f'
-let newString = ""
-let array = string.split(",")
-
-for(let i = 0; i < array.length; i++){
-       newString += array[i] + i + " "
-    }
-console.log(newString)
+console.log(string.split(",").map((letter)=>{return letter + string.split(",").indexOf(letter)}).toString())
