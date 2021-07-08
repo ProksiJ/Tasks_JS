c467b0168865e94a1fd6f5fa09a4c163b06337e7
Tomasheva Ksenia <82093959+ProksiJ@users.noreply.github.com>
2021-07-08 23:12:22 +0300

    use Set

--- task14.js
+++ task14.js
@@ -1,27 +1,13 @@
 function union(arr1, arr2){
-    /*let unionArray = []
-    arr1 = arr1.concat(arr2)
-    arr1.forEach(element => {
-        if(!unionArray.includes(element)) unionArray.push(element)
-    })
-    return unionArray*/
-    return new Set(arr1.concat(arr2)) 
+    return new Set([...arr1, ...arr2]) 
 }
 
 function intersection(arr1,arr2){
-    let intersectionArray = []
-    arr1.forEach(element => {
-        if(arr2.includes(element)) intersectionArray.push(element)
-    })
-    return intersectionArray
+    return new Set (arr1.filter((element) =>arr2.includes(element)))
 }
 
 function difference(arr1,arr2){
-    let differenceArray = []
-    arr1.forEach(element => {
-        if(!arr2.includes(element) && !differenceArray.includes(element)) differenceArray.push(element)
-    })
-    return differenceArray
+    return new Set(arr1.filter((element) => !arr2.includes(element)))
 }
 
 console.log(union([4,5,7,2,1,5],[1,2,3,7,9]))
